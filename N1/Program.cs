c3f595724d2f25c8fe965af032550da36c4f4473
lolmem745 <93839006+lolmem745@users.noreply.github.com>
2022-10-19 04:39:05 +0300

    First problem solved

--- N1/Program.cs
+++ N1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32( Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32( Console.ReadLine());
+if (a>=b){
+    Console.WriteLine($"max = {a}");
+}
+else{
+    Console.WriteLine($"max = {b}");
+}
