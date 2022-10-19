56f2b0a8ec85bb885bd68d702de9ebf4fe57a4c1
lolmem745 <93839006+lolmem745@users.noreply.github.com>
2022-10-19 05:09:02 +0300

    Third problem solved

--- N3/Program.cs
+++ N3/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите целое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Четное ли число? -> ");
+int b = a % 2;
+if (b == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
